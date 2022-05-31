f6a5a4dae97637f08fc95af3d5ff2c6b25d8a93d
Ubuntu <ubuntu@ip-172-10-59-146.ap-northeast-2.compute.internal>
2022-05-31 04:59:50 +0000

    ver0.1

---
+++ src/main/java/com.test.app/App.java
@@ -0,0 +1,17 @@
+package com.test.app;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class App {
+    @RequestMapping("/hello")
+    String home() {
+        return "Hello World! test_app!";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
