07fcefe50274a17c8edc12fe6b1bca0bff97a1e3
Seunghan Ga <seunghan@Seunghanui-MacBookPro.local>
2022-06-03 11:07:41 +0900

    test2

--- src/main/java/com.test.app/App.java
+++ src/main/java/com.test.app/App.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 public class App {
     @RequestMapping("/hello")
     String home() {
-        return "Hello World! test_app!";
+        return "Hello World! test_app! 2222";
     }
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
